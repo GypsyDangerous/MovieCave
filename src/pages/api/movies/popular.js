5617ea2422b7417c6bdf67d24bae675a49aad644
David Snyder <davidgraygs4@gmail.com>
2020-09-23 08:40:10 -0400

    added first few api links, basically wrappers for the moviedb api

---
+++ src/pages/api/movies/popular.js
@@ -0,0 +1,6 @@
+import fetch from "fetchio-js";
+
+export default async (req, res) => {
+	const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&page=${req.query.page}`);
+	res.json(data);
+};
