c76e9c9c8192fc1225eed05d9fa6fe071eb358a8
David Snyder <davidgraygs4@gmail.com>
2020-09-23 10:59:24 -0400

    added CSP

--- src/components/shared/SEO.js
+++ src/components/shared/SEO.js
@@ -5,6 +5,7 @@ const SEO = ({ title }) => {
 		<Head>
 			<title>{title}</title>
 			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			<meta httpEquiv="Content-Security-Policy" content=""/>
 		</Head>
 	);
 };
